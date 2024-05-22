73e588c772a8fa625c55d430e23945c41ebe4fe1
melih.akbulut <melih.akbulut@defne.com.tr>
2024-05-22 14:21:01 +0300

    test

--- src/main/java/RedisHandler.java
+++ src/main/java/RedisHandler.java
@@ -13,6 +13,7 @@ public class RedisHandler implements Runnable {
     public void run() {
         try {
             clientSocket.getOutputStream().write("+PONG\r\n".getBytes());
+            Thread.sleep(100);
             clientSocket.getOutputStream().write("+PONG\r\n".getBytes());
         } catch (Exception e) {
             // TODO Auto-generated catch block
