dc073612384ebfec9060094b3f5eac54dd579583
melih.akbulut <melih.akbulut@defne.com.tr>
2024-05-24 17:36:57 +0300

    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,11 +6,17 @@ public class Main {
     public static void main(String[] args) {
         // You can use print statements as follows for debugging, they'll be visible when running tests.
         System.out.println("Logs from your program will appear here!");
+        Integer port = null;
+        for (int i = 0; i < args.length; i++) {
+            if (args[i].equals("--port")) {
+                port = Integer.valueOf(args[i + 1]);
+                break;
+            }
 
+        }
         //  Uncomment this block to pass the first stage
         ServerSocket serverSocket = null;
         Socket clientSocket = null;
-        int port = 6379;
         try {
             serverSocket = new ServerSocket(port);
             serverSocket.setReuseAddress(true);
