97f11da9b6368664aa441fad7fe818be80220377
melih.akbulut <melih.akbulut@defne.com.tr>
2024-05-24 16:00:13 +0300

    test

--- src/main/java/RedisHandler.java
+++ src/main/java/RedisHandler.java
@@ -1,9 +1,13 @@
 import java.io.IOException;
 import java.net.Socket;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
 
 public class RedisHandler implements Runnable {
 
     private Socket clientSocket;
+    private Deque<String> commandQueue = new ArrayDeque<String>();
 
     public RedisHandler(Socket clientSocket) {
         this.clientSocket = clientSocket;
@@ -12,30 +16,56 @@ public class RedisHandler implements Runnable {
     @Override
     public void run() {
         try {
-            clientSocket.getOutputStream().write("+PONG\r\n".getBytes());
-            Thread.sleep(200);
-            clientSocket.getOutputStream().write("+PONG\r\n".getBytes());
-            Thread.sleep(200);
-            clientSocket.getOutputStream().write("+PONG\r\n".getBytes());
+            listenSocket();
         } catch (Exception e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
     }
 
-    public static void listen(Socket clientSocket) throws IOException {
-        final int bufLen = 128 * 0x400; // 4KB
-        byte[] buf = new byte[bufLen];
-        int index = 0;
+    public void listenSocket() throws IOException {
         while (true) {
             byte b = (byte) clientSocket.getInputStream().read();
-            if (b == -1)
-                break;
-            buf[index] = b;
-
-            index++;
+            if (b == '*') {
+                int commandWordLength = Integer.valueOf(getStringValueOfByte((byte) clientSocket
+                                .getInputStream().read()));
+                skipNewLine();
+                String[] commandWords = new String[commandWordLength];
+                for (int i = 0; i < commandWordLength; i++) {
+                    commandWords[i] = parseCommand();
+                }
+                System.out.println(Arrays.toString(commandWords));
+                handle(commandWords);
+            }
         }
-
     }
 
+    private void handle(String[] commandWords) throws IOException {
+        String message = null;
+        if (commandWords[0].toLowerCase().equals("ping")) {
+            message = "+PONG\r\n";
+        } else if (commandWords[0].toLowerCase().equals("echo")) {
+            message = String.format("$3\r\n%s\r\n", commandWords[1]);
+
+        }
+        clientSocket.getOutputStream().write(message.getBytes());
+    }
+
+    private String parseCommand() throws IOException {
+        byte wordValue = (byte) clientSocket.getInputStream().read();
+        int wordLength = Integer
+                        .valueOf(getStringValueOfByte((byte) clientSocket.getInputStream().read()));
+        skipNewLine();
+        String command = new String(clientSocket.getInputStream().readNBytes(wordLength));
+        skipNewLine();
+        return command;
+    }
+
+    private void skipNewLine() throws IOException {
+        clientSocket.getInputStream().readNBytes(2);
+    }
+
+    private String getStringValueOfByte(byte b) {
+        return new String(new byte[] {b});
+    }
 }
