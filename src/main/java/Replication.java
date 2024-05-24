60abb8c0c1b385d51b5a836b8183b36577f97f56
melih.akbulut <melih.akbulut@defne.com.tr>
2024-05-24 18:06:32 +0300

    test

--- src/main/java/Replication.java
+++ src/main/java/Replication.java
@@ -12,14 +12,14 @@ public class Replication {
 
     public Replication() {
         keyValueMap.put("role", "master");
-        keyValueMap.put("connected_slaves", 0);
-        keyValueMap.put("master_replid", "8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb");
-        keyValueMap.put("master_repl_offset", 0);
-        keyValueMap.put("second_repl_offset", -1);
-        keyValueMap.put("repl_backlog_active", 0);
-        keyValueMap.put("repl_backlog_size", 1048576);
-        keyValueMap.put("repl_backlog_first_byte_offset", 0);
-        keyValueMap.put("repl_backlog_histlen", "");
+        //        keyValueMap.put("connected_slaves", 0);
+        //        keyValueMap.put("master_replid", "8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb");
+        //        keyValueMap.put("master_repl_offset", 0);
+        //        keyValueMap.put("second_repl_offset", -1);
+        //        keyValueMap.put("repl_backlog_active", 0);
+        //        keyValueMap.put("repl_backlog_size", 1048576);
+        //        keyValueMap.put("repl_backlog_first_byte_offset", 0);
+        //        keyValueMap.put("repl_backlog_histlen", "");
     }
 
 }
