94c727d1c6aa2c5f4a05a48e26ede84a1a4b108c
melih.akbulut <melih.akbulut@defne.com.tr>
2024-05-26 16:05:18 +0300

    test

--- src/main/java/Replication.java
+++ src/main/java/Replication.java
@@ -56,6 +56,7 @@ public class Replication {
             String psync = "*3\r\n$5\r\nPSYNC\r\n$1\r\n?\r\n$2\r\n-1\r\n";
             sendMessage(socket, psync);
             handle("psync", parseCommand(socket));
+            socket.close();
         }
     }
 
