43bd337bb1dc54c425d400f28053319a9aa338a4
melih.akbulut <melih.akbulut@defne.com.tr>
2024-05-28 18:07:27 +0300

    test

--- src/main/java/RedisStream.java
+++ src/main/java/RedisStream.java
@@ -7,8 +7,6 @@ public class RedisStream {
 
     //time,id
     private Map<Long, List<Long>> stream = new ConcurrentHashMap<Long, List<Long>>();
-    private long lastID = -1;
-    private long lastIndex = 0;
     //time+index, Pair
     private Map<String, Pair> streamValues = new ConcurrentHashMap<String, Pair>();
 
@@ -19,28 +17,37 @@ public class RedisStream {
         }
         String[] idArr = id.split("-");
         Long ms = Long.parseLong(idArr[0]);
-        Long index = null;
-        if (!idArr[1].equals("*")) {
-            index = Long.parseLong(idArr[1]);
-        } else {
-            index = lastIndex + 1;
+        long lastIndex = 0;
+        List<Long> indexes = stream.get(ms);
+        if (indexes == null) {
+            indexes = new CopyOnWriteArrayList<Long>();
+            stream.put(ms, indexes);
         }
-        List<Long> indexList = stream.get(index);
+        if (!indexes.isEmpty())
+            lastIndex = indexes.get(indexes.size() - 1);
+        else {
+            if (ms == 0)
+                lastIndex = 0;
+            else
+                lastIndex = -1;
+        }
+        boolean autoInc = idArr[1].equals("*");
 
-        if (!idArr[1].equals("*") && lastID >= Long.parseLong(id.replaceAll("-", ""))) {
-            throw new IllegalArgumentException(
-                            "ERR The ID specified in XADD is equal or smaller than the target stream top item");
+        if (!autoInc) {
+            Long sentIndex = Long.parseLong(idArr[1]);
+
+            if (sentIndex <= lastIndex) {
+                throw new IllegalArgumentException(
+                                "ERR The ID specified in XADD is equal or smaller than the target stream top item");
+            }
 
         }
 
-        if (indexList == null)
-            indexList = new CopyOnWriteArrayList<Long>();
-        indexList.add(index);
-        stream.put(ms, indexList);
-        lastID = ms + index;
-        streamValues.put(ms + "-" + index, Pair.builder().key(key).value(value).build());
-        lastIndex = index;
-        return ms + "-" + index;
+        Long nextIndex = lastIndex + 1;
+        indexes.add(nextIndex);
+        stream.put(ms, indexes);
+        streamValues.put(ms + "-" + nextIndex, Pair.builder().key(key).value(value).build());
+        return ms + "-" + nextIndex;
 
     }
 
