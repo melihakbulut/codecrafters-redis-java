f1ff7aab56de01fff2f840da3f699269520fd1b7
melih.akbulut <melih.akbulut@defne.com.tr>
2024-05-28 17:00:17 +0300

    tests

--- src/main/java/RedisStream.java
+++ src/main/java/RedisStream.java
@@ -7,6 +7,7 @@ public class RedisStream {
 
     //time,id
     private Map<Long, List<Long>> stream = new ConcurrentHashMap<Long, List<Long>>();
+    private long lastID = -1;
     private long lastIndex = -1;
     //time+index, Pair
     private Map<String, Pair> streamValues = new ConcurrentHashMap<String, Pair>();
@@ -18,10 +19,15 @@ public class RedisStream {
         }
         String[] idArr = id.split("-");
         Long ms = Long.parseLong(idArr[0]);
-        Long index = Long.parseLong(idArr[1]);
+        Long index = null;
+        if (idArr[1] != "*") {
+            index = Long.parseLong(idArr[1]);
+        } else {
+            index = lastIndex + 1;
+        }
         List<Long> indexList = stream.get(index);
 
-        if (lastIndex >= Long.parseLong(id.replaceAll("-", ""))) {
+        if (lastID >= Long.parseLong(id.replaceAll("-", ""))) {
             throw new IllegalArgumentException(
                             "ERR The ID specified in XADD is equal or smaller than the target stream top item");
 
@@ -29,10 +35,12 @@ public class RedisStream {
 
         if (indexList == null)
             indexList = new CopyOnWriteArrayList<Long>();
-
+        indexList.add(index);
         stream.put(ms, indexList);
         streamValues.put(id, Pair.builder().key(key).value(value).build());
-        lastIndex = Long.parseLong(id.replaceAll("-", ""));
+        lastID = Long.parseLong(id.replaceAll("-", ""));
+        lastIndex = index;
+
     }
 
 }
