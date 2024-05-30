87f5d6ffd5ccca4bbddd8b0215ce11ae33658d93
melih.akbulut <melih.akbulut@defne.com.tr>
2024-05-30 15:42:38 +0300

    test

--- src/main/java/RedisHandler.java
+++ src/main/java/RedisHandler.java
@@ -250,7 +250,7 @@ public class RedisHandler implements Runnable {
                 }
             }
             if (!anyData) {
-                message = "_\r\n";
+                message = "-_\r\n";
             } else {
                 message = String.format("*%s\r\n", xRangeResults.size());
                 for (XRange xRange : xRangeResults) {
